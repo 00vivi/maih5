dd45f9cd78b0be49c47068aee27d7c38298aafcc
dell <fl19372@gmail.com>
2019-03-04 21:31:56 +0800

    modify

--- 004MouseEvent/FingerGuess/main.js
+++ 004MouseEvent/FingerGuess/main.js
@@ -1,58 +1,93 @@
-var r;
+var r,score=0,grade=3,lastHumanChoice;
+var isComputerWin=false,lastComputerChoice;
 function rock() {
-    r = 3 * Math.random();
-    document.getElementById("myChoice").innerHTML="<img src=\"images/rock.png\"/>;"
-    var computerResult=computerChoice();
-    judge("rock",computerResult);
+    document.getElementById("myChoice").innerHTML="<img src=\"images/rock.png\"/>";
+    judge("rock");
+    lastHumanChoice="rock";
 }
 function paper() {
-    r = 3 * Math.random();
-    document.getElementById("myChoice").innerHTML="<img src=\"images/paper.png\"/>;"
-    var computerResult=computerChoice();
-    judge("paper",computerResult);
+    document.getElementById("myChoice").innerHTML="<img src=\"images/paper.png\"/>";
+    judge("paper");
+    lastHumanChoice="paper";
 }
 function scissors() {
-    r = 3 * Math.random();
-    document.getElementById("myChoice").innerHTML="<img src=\"images/scissors.png\"/>;"
-    var computerResult=computerChoice();
-    judge("scissors",computerResult);
+    document.getElementById("myChoice").innerHTML="<img src=\"images/scissors.png\"/>";
+    judge("scissors");
+    lastHumanChoice="scissors";
 }
-function judge(myChoice, computerResult) {
+function judge(myChoice) {
+    r = 3 * Math.random();
+    var computerResult;
+    if(grade==1){
+        computerResult = onlyRocker();
+    }
+    else if(grade==2){
+        computerResult = learnFromHuman();
+    }
+    else if(grade==3){
+        computerResult = winnerAgain();
+        lastComputerChoice=computerResult;
+    }
+    else{
+        computerResult=computerChoice()
+    }
     if(myChoice=="rock"){
         if (computerResult=="rock"){
             document.getElementById("result").innerHTML = "draw";
+            isComputerWin=false;
         }
         else if(computerResult=="paper"){
             document.getElementById("result").innerHTML = "you lose";
+            score-=1;
+            isComputerWin=true;
         }
         else{
             document.getElementById("result").innerHTML = "you win";
+            score+=1;
+            isComputerWin=false;
         }
     }
     else if(myChoice=="paper"){
         if (computerResult=="rock"){
             document.getElementById("result").innerHTML = "you win";
+            score+=1;
+            isComputerWin=false;
+
         }
         else if(computerResult=="paper"){
             document.getElementById("result").innerHTML = "draw";
+            isComputerWin=false;
         }
         else{
             document.getElementById("result").innerHTML = "you lose";
+            score-=1;
+            isComputerWin=true;
         }
     }
     else{
         if (computerResult=="rock"){
             document.getElementById("result").innerHTML = "you lose";
+            score-=1;
+            isComputerWin=true;
         }
         else if(computerResult=="paper"){
             document.getElementById("result").innerHTML = "you win";
+            isComputerWin=false;
+            score+=1;
         }
         else{
             document.getElementById("result").innerHTML = "draw";
+            isComputerWin=false;
         }
     }
-}
-function go() {
+    if(score>=5){
+        score=0;
+        grade+=1;
+    }
+    document.getElementById("result").innerHTML="the "+grade+" level, the score is "+score;
+    if(grade>=10){
+        document.getElementById("result").innerHTML="NB,PASS  "+score;
+    }
 }
 function computerChoice() {
     if (r < 1) {
@@ -65,4 +100,23 @@ function computerChoice() {
         document.getElementById("computerChoice").innerHTML = "<img src='images/paper.png'>";
         return "paper"
     }
+}
+function onlyRocker() {
+    document.getElementById("computerChoice").innerHTML="<img src='images/rock.png'>";
+    return "rock";
+}
+function learnFromHuman() {
+    document.getElementById("computerName").innerHTML="模仿帝";
+    document.getElementById("computerChoice").innerHTML="<img src='images/"+lastHumanChoice+".png'>";
+    return lastHumanChoice;
+}
+function winnerAgain() {
+    document.getElementById("computerName").innerHTML="赢了还出";
+    if(isComputerWin){
+        document.getElementById("computerChoice").innerHTML="<img src='images/"+lastComputerChoice+".png'>";
+        return lastComputerChoice;
+    }
+    var temp=computerChoice();
+    document.getElementById("computerChoice").innerHTML="<img src='images/"+temp+".png'>";
+    return temp;
 }
